08ffa839bb3e1bc8d11e52eab6dc29865490d728
Cara Hill <cara@rabidtech.co.nz>
2018-12-20 10:50:24 +1300

    Updated mutation type, added create link resolver.

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,11 +1,7 @@
-Types::MutationType = GraphQL::ObjectType.define do
-  name "Mutation"
+# frozen_string_literal: true
 
-  # TODO: Remove me
-  field :testField, types.String do
-    description "An example field added by the generator"
-    resolve ->(obj, args, ctx) {
-      "Hello World!"
-    }
-  end
+Types::MutationType = GraphQL::ObjectType.define do
+  name 'Mutation'
+
+  field :createLink, function: Resolvers::CreateLink.new
 end
